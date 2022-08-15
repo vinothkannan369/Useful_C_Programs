4e0fd100af63882b9c88989ed8347d482284a1e6
Vinothkannan R <108211178+vinothkannan369@users.noreply.github.com>
2022-08-15 17:02:29 +0530

    Delete File_Handling.h

--- File_Handling/File_Handling.h
+++
@@ -1,9 +0,0 @@
-
-
-
-int get_file_size(const char* file_name);
-int append_file(const char* file_name,const char* data);
-int file_line_count(const char* file_name,int file_size );
-int read_nth_line(const char* file_name,int linenumber,char *save_data);
-int print_all_line(const char* file_name);
-int delete_line(const char* file_name,int line_number);
